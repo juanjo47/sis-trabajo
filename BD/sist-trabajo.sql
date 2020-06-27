c7c33e2ddfb7c4c967bea7209f902385db649016
juanjo47 <lover_drago@hotmail.com>
2020-06-26 22:52:39 -0400

    adm commit2

--- BD/sist-trabajo.sql
+++ BD/sist-trabajo.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 18-06-2020 a las 06:15:43
+-- Tiempo de generación: 27-06-2020 a las 04:51:20
 -- Versión del servidor: 10.4.11-MariaDB
 -- Versión de PHP: 7.3.18
 
@@ -33,15 +33,17 @@ CREATE TABLE `administrador` (
   `contraseña` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
   `nombres` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
   `carnet` varchar(15) COLLATE utf8_unicode_ci NOT NULL,
-  `codigo_saga` varchar(20) COLLATE utf8_unicode_ci NOT NULL
+  `codigo_saga` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
+  `nivel` int(2) NOT NULL,
+  `estado` int(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Volcado de datos para la tabla `administrador`
 --
 
-INSERT INTO `administrador` (`ID_admin`, `correo`, `contraseña`, `nombres`, `carnet`, `codigo_saga`) VALUES
-(4, 'admin@gmail.com', '21232f297a57a5a743894a0e4a801fc3', 'Juan Jose ', 'Quiroga Torrez', 'A17615-6');
+INSERT INTO `administrador` (`ID_admin`, `correo`, `contraseña`, `nombres`, `carnet`, `codigo_saga`, `nivel`, `estado`) VALUES
+(4, 'admin@gmail.com', '21232f297a57a5a743894a0e4a801fc3', 'Juan Jose Quiroga Torrez', '7376263', 'A17615-6', 1, 1);
 
 -- --------------------------------------------------------
 
@@ -61,8 +63,17 @@ CREATE TABLE `docente` (
   `especialidad_3` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
   `especialidad_4` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
   `especialidad_5` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-  `trabajo` int(15) NOT NULL
+  `trabajo` int(15) DEFAULT NULL,
+  `estado` int(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `docente`
+--
+
+INSERT INTO `docente` (`ID_docente`, `correo`, `contraseña`, `nombres`, `codigo_saga`, `carnet`, `especialidad_1`, `especialidad_2`, `especialidad_3`, `especialidad_4`, `especialidad_5`, `trabajo`, `estado`) VALUES
+(6, 'storot@gmail.com', 'e10adc3949ba59abbe56e057f20f883e', 'sergio toro ', '485221', '15652154', 'ia', 'bd', 'gestio', 'IS', 'ma', NULL, 1),
+(7, 'gmagueno@gmail.com', '348f0f4edf209b54d51996137488daae', 'grover magueño', '985749asfa', '454544', 'ia', 'bd', 'gestio', 'IS', 'ma', NULL, 1);
 
 -- --------------------------------------------------------
 
@@ -78,11 +89,20 @@ CREATE TABLE `estudiante` (
   `semestre` varchar(15) COLLATE utf8_unicode_ci NOT NULL,
   `codigo_saga` varchar(15) COLLATE utf8_unicode_ci NOT NULL,
   `carnet` varchar(15) COLLATE utf8_unicode_ci NOT NULL,
-  `tutor` int(100) NOT NULL,
-  `revisor_1` int(100) NOT NULL,
-  `revisor_2` int(100) NOT NULL,
-  `trabajo` int(100) NOT NULL
+  `tutor` int(100) DEFAULT NULL,
+  `revisor_1` int(100) DEFAULT NULL,
+  `revisor_2` int(100) DEFAULT NULL,
+  `trabajo` int(100) DEFAULT NULL,
+  `estado` int(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `estudiante`
+--
+
+INSERT INTO `estudiante` (`ID_estudiante`, `correo`, `contraseña`, `nombres`, `semestre`, `codigo_saga`, `carnet`, `tutor`, `revisor_1`, `revisor_2`, `trabajo`, `estado`) VALUES
+(4, 'jquirogat@est.emi.edu.bo', 'e4e4564027d73a4325024d948d167e93', 'Juan Jose Quiroga Torrez', 'Noveno', 'A17913-2', '7376263', NULL, NULL, NULL, NULL, 1),
+(6, 'storot@gmail.com', '12345678', 'sergio toro ', 'Decimo', 'ma', '15652154', NULL, NULL, NULL, NULL, 1);
 
 -- --------------------------------------------------------
 
@@ -145,13 +165,13 @@ ALTER TABLE `administrador`
 -- AUTO_INCREMENT de la tabla `docente`
 --
 ALTER TABLE `docente`
-  MODIFY `ID_docente` int(100) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID_docente` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT de la tabla `estudiante`
 --
 ALTER TABLE `estudiante`
-  MODIFY `ID_estudiante` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `ID_estudiante` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT de la tabla `trabajo`
@@ -182,7 +202,7 @@ ALTER TABLE `estudiante`
 -- Filtros para la tabla `trabajo`
 --
 ALTER TABLE `trabajo`
-  ADD CONSTRAINT `trabajo_ibfk_1` FOREIGN KEY (`ID_trabajo`) REFERENCES `estudiante` (`Trabajo`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `trabajo_ibfk_1` FOREIGN KEY (`ID_trabajo`) REFERENCES `estudiante` (`trabajo`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
